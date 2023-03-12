63689201207bf17b558baa4d55fc4b110c4e5ade
Shubham Kondekar <kondekarshubham123@gmail.com>
2023-03-12 22:44:26 +0530

    API data access from response list

--- src/App.js
+++ src/App.js
@@ -3,16 +3,37 @@ import Item from './components/Item';
 import ItemDate from './components/ItemDate';
 
 function App() {
+  const response = 
+    [
+      {
+        itemName:"Nirma",
+        itemDate: "20",
+        itemMonth: "June",
+        ItemYear: "1998"
+      }, 
+      {
+        itemName:"SurfExcel",
+        itemDate: "22",
+        itemMonth: "July",
+        ItemYear: "2000"
+      }, 
+      {
+        itemName:"555",
+        itemDate: "10",
+        itemMonth: "Sept",
+        ItemYear: "2010"
+      }];
+
   return (
     <div>
-      <Item name="Nirma"></Item>
-      <ItemDate day="20" month="June" year="1998"></ItemDate>
+      <Item name={response[0].itemName}></Item>
+      <ItemDate day={response[0].itemDate} month={response[0].itemMonth} year={response[0].ItemYear}></ItemDate>
 
-      <Item name="SurfExcel"></Item>
-      <ItemDate day="22" month="July" year="2000"></ItemDate>
+      <Item name={response[1].itemName}></Item>
+      <ItemDate day={response[1].itemDate} month={response[1].itemMonth} year={response[1].ItemYear}></ItemDate>
 
-      <Item name="555"></Item>
-      <ItemDate day="20" month="Sept" year="2010"></ItemDate>
+      <Item name={response[2].itemName}></Item>
+      <ItemDate day={response[2].itemDate} month={response[2].itemMonth} year={response[2].ItemYear}></ItemDate>
 
       <div className="App">Hello jee</div>
 
