48954c8be8ac1ee06023c6bdc3f718f354f9a1ac
Shubham Kondekar <kondekarshubham123@gmail.com>
2023-03-12 22:37:45 +0530

    Send data using props

--- src/App.js
+++ src/App.js
@@ -1,11 +1,21 @@
 import './App.css';
 import Item from './components/Item';
+import ItemDate from './components/ItemDate';
 
 function App() {
   return (
     <div>
-      <Item></Item>
+      <Item name="Nirma"></Item>
+      <ItemDate day="20" month="June" year="1998"></ItemDate>
+
+      <Item name="SurfExcel"></Item>
+      <ItemDate day="22" month="July" year="2000"></ItemDate>
+
+      <Item name="555"></Item>
+      <ItemDate day="20" month="Sept" year="2010"></ItemDate>
+
       <div className="App">Hello jee</div>
+
     </div>
   );
 }
